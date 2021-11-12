0934894dc694c248be4f71470a38a831e8c3d051
Jaime <jaimecr72@gmail.com>
2021-11-12 15:40:27 +0000

    basic functionality done

--- src/main.py
+++ src/main.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Planet, FavPeople, FavPlanet
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -30,14 +29,91 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_people():
+    all_people = People.query.all()
+    all_people = list(map(lambda x: x.serialize(), all_people))
+    return jsonify(all_people), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_n(people_id):
+    person = People.query.get(people_id)
+    return jsonify(person.serialize())
 
-    return jsonify(response_body), 200
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    all_planets = Planet.query.all()
+    all_planets = list(map(lambda x: x.serialize(), all_planets))
+    return jsonify(all_planets), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet_n(planet_id):
+    myplanet = Planet.query.get(planet_id)
+    return jsonify(myplanet.serialize()), 200
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), all_users))
+    return jsonify(all_users), 200
+
+@app.route('/users/favorites', methods=['GET'])
+def get_favorites_user():
+    activ_user = User.query.filter_by(is_active=True).first()
+    if activ_user is None:
+        return jsonify({"msg": "No user active"}), 401
+    else:
+        fav_people = FavPeople.query.filter_by(idUser=activ_user.id)
+        all_fav = []
+        for fav in fav_people:
+            p = People.query.filter_by(id=fav.idPeople).first()
+            all_fav.append(p.serialize())
+        fav_planet = FavPlanet.query.filter_by(idUser=activ_user.id)
+        for fav in fav_planet:
+            p = Planet.query.filter_by(id=fav.idPlanet).first()
+            all_fav.append(p.serialize())
+
+        return jsonify(all_fav), 200
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST','DELETE'])
+def add_favplanet_user(planet_id):
+    activ_user = User.query.filter_by(is_active=True).first()
+    if activ_user is None:
+        return jsonify({"msg": "No user active"}), 401
+    else:
+        if request.method == 'POST':
+            favplanet = FavPlanet(idUser=activ_user.id, idPlanet=planet_id)
+            db.session.add(favplanet)
+            db.session.commit()
+            return jsonify({"favorite planet added": activ_user.email+" - "+str(planet_id)})
+        else:
+            favplanet = FavPlanet.query.filter_by(idUser=activ_user.id, idPlanet=planet_id).first()
+            if favplanet is None:
+                return jsonify({"error":"favorite not exists"}), 401
+            else:
+                db.session.delete(favplanet)
+                db.session.commit()
+                return jsonify({"favorite planet deleted": activ_user.email+" - "+str(planet_id)})
+        
+@app.route('/favorite/people/<int:people_id>', methods=['POST','DELETE'])
+def add_favpeople_user(people_id):
+    activ_user = User.query.filter_by(is_active=True).first()
+    if activ_user is None:
+        return jsonify({"msg": "No user active"}), 401
+    else:
+        if request.method == 'POST':
+            favpeople = FavPeople(idUser=activ_user.id, idPeople=people_id)
+            db.session.add(favpeople)
+            db.session.commit()
+            return jsonify({"favorite people added":activ_user.email+" - "+str(people_id)})
+        else:
+            favpeople = FavPeople.query.filter_by(idUser=activ_user.id, idPeople=people_id).first()
+            if favpeople is None:
+                return jsonify({"error":"favorite not exists"}), 401
+            else:
+                db.session.delete(favpeople)
+                db.session.commit()
+                return jsonify({"favorite people deleted": activ_user.email+" - "+str(people_id)})
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,13 +7,81 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favPeople = db.relationship('FavPeople', backref='user', lazy=True)
+    favPlanet = db.relationship('FavPlanet', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(50), nullable=True)
+    haircolor = db.Column(db.String(50), nullable=True)
+    eyescolor = db.Column(db.String(50), nullable=True)
+    favPeople = db.relationship('FavPeople', backref='people', lazy=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "haircolor": self.haircolor,
+            "eyescolor": self.eyescolor
+        }
+
+class Planet(db.Model):
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    population = db.Column(db.Integer, nullable=True)
+    terrain = db.Column(db.String(50), nullable=True)
+    favPlanet = db.relationship('FavPlanet', backref='planet', lazy=True)
+    
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain
+        }
+
+class FavPeople(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idUser = db.Column(db.Integer, db.ForeignKey('user.id'))
+    idPeople = db.Column(db.Integer, db.ForeignKey('people.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "idUser": self.idUser,
+            "idPeople": self.idPeople
+        }
+
+class FavPlanet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idUser = db.Column(db.Integer, db.ForeignKey('user.id'))
+    idPlanet = db.Column(db.Integer, db.ForeignKey('planet.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "idUser": self.idUser,
+            "idPlanet": self.idPlanet
         }
